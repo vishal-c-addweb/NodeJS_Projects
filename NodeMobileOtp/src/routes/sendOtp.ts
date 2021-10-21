76c60d0e5b8423b14c8f58268635023872e55048
vishal-c-addweb <vishal.c@addwebsolution.in>
2021-10-21 12:29:33 +0530

    change in otp service

--- NodeMobileOtp/src/routes/sendOtp.ts
+++ NodeMobileOtp/src/routes/sendOtp.ts
@@ -2,17 +2,14 @@ import { Router } from "express";
 import sendOtpController from "../controller/sendOtpController";
 const router: Router = Router();
 
-// @route   POST user
-// @desc    Give Mobile number, returns the token upon successful registration or login.
-// @access  Public
-router.post("/sendotp",sendOtpController.loginRegister);
+// @route   POST sendotp
+// @desc    Give Mobile number, returns the otp.
+// @access  Private with springedge api-key
+router.post("/sendotp",sendOtpController.sendOtp);
 
-// @route   GET user
-// @access  Private
-router.get("/sendotp", sendOtpController.userData);
-
-// @route   POST user
-// @desc    Give Mobile number, returns the token upon successful registration or login.
+// @route   POST verify
+// @desc    verify otp and return proper message.
 // @access  Public
 router.post("/verify",sendOtpController.verify);
+
 export default router;
